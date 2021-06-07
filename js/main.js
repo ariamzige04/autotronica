b280596a3daaa1b04322a3912c8c3e0f8157bff7
ariamzige04 <74326414+ariamzige04@users.noreply.github.com>
2021-06-07 15:28:01 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-const hamburguesa=document.querySelector("#hamburguesa"),navegacion=document.querySelector(".nav");hamburguesa.addEventListener("click",()=>{navegacion.classList.toggle("muestra"),navegacion.style.transition="transform .7s ease",hamburguesa.classList.toggle("hamburguesaAnimacion")}),window.addEventListener("load",()=>{const e=document.querySelector(".loader");e.style.opacity=0,e.style.visibility="hidden"});
+const hamburguesa = document.querySelector("#hamburguesa");
+const navegacion = document.querySelector(".nav");
+hamburguesa.addEventListener("click", () => {
+    navegacion.classList.toggle("muestra");
+    navegacion.style.transition = "transform .7s ease";
+
+    hamburguesa.classList.toggle("hamburguesaAnimacion");
+});
+
+window.addEventListener("load", () => {
+    const cargador = document.querySelector(".loader");
+    cargador.style.opacity = 0;
+    cargador.style.visibility = "hidden";
+})
