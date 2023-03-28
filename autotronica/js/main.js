6eedad0dfc27ee74ef3bac5fb31ead1e9167b785
Ari Gómez <gomezari67@gmail.com>
2023-03-28 13:45:51 -0600

    Delete autotronica directory

--- autotronica/js/main.js
+++
@@ -1,8 +0,0 @@
-const hamburguesa = document.querySelector("#hamburguesa");
-const navegacion = document.querySelector(".nav");
-hamburguesa.addEventListener("click", () => {
-    navegacion.classList.toggle("muestra");
-    navegacion.style.transition = "transform .7s ease";
-
-    hamburguesa.classList.toggle("hamburguesaAnimacion");
-});
